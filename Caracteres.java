5b9ce27846c939463c0b70094175551c1566afe0
Usiel-Curt <20243ds143@utez.edu.mx>
2025-05-08 09:01:24 -0600

    Caracteres

---
+++ Caracteres.java
@@ -0,0 +1,11 @@
+public class Caracteres{
+    public static void main(String[] args){
+        char letra = 'a';
+        char simbolo = '@';
+        char numero = '3';
+
+        System.out.println("letra =" + letra);
+        System.out.println("simbolo =" + simbolo);
+        System.out.println("numero =" + numero);
+    }
+}
